fe59328baea94d915e506f85b7f9d05f96507ca5
William <80454082+the-little-way@users.noreply.github.com>
2021-08-06 10:43:14 +0000

    v1.1 added some styles and ejs

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser');
 const https = require('https');
 
 
-app.use( bodyParser.urlencoded( {extended:true}) );
+app.use( bodyParser.urlencoded( {extended:true} ) );
 
+app.set('view engine', 'ejs');
 
 app.listen(3000);
 
@@ -16,151 +17,83 @@ const cardCSS = "background-color: aliceblue; margin: 20px auto; width: 300px; f
 
 // routing images
 // Define the static file path. app.use() method mounts the middleware express.static
-app.use(express.static(__dirname+'/'));
+app.use(express.static('assets'));
 
 
 app.get('/', (req, res)=> {
 	
 	console.log("server online..");
 
-	res.sendFile( __dirname + '\\index.html') //using backslash in path due to windows file directory syntax
+	res.sendFile( __dirname + '/index.html')
 });
 
 
-// weather response if option Accra is clicked
-app.get('/accra', (req, res) => {
+// general purpose function to check weather based on city input as argument
+function checkWeather(ar, req, res) {
 	
-	const City = "Accra";
-	
-	const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Accra,gh&units=metric&APPID=a0562d44b2bd5392dc0f1ef5d322cc65";
-
-	var cityTemp = "testing..";
+	const City = ar;
+	const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ City + "&units=metric&APPID=a0562d44b2bd5392dc0f1ef5d322cc65";
 	
 	// send get request and receive json from api server and check the status code
 	https.get( apiUrl, (response) => {
 	console.log( response.statusCode )
-
-		//parse it into JSON
-		response.on("data", (data) => {
-			const weatherData = JSON.parse(data);
-			cityTemp = weatherData.main.temp;
-		//	console.log( weatherData );
-		//	console.log( cityTemp );
-		//	return cityTemp;
-			res.send(`
-			<div style="${cardCSS}">
-			The temperature in ${City} is:
-			<br/>${cityTemp}
-			<br/>degrees  Celcius
-			<p><a href="/">Click here to go back</a></p>
-			</div>
-			`)
-		});
+	
+		if(response.statusCode == 200){
+			//parse it into JSON, reformat to 1 decimal point
+			response.on("data", (data) => {
+					const weatherData = JSON.parse(data);
+					let desc = weatherData.weather[0].description;
+					let icon = weatherData.weather[0].icon;
+					let iconURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+					let cityTempVal = weatherData.main.temp;
+					let cityTemp = Math.round(cityTempVal * 10) / 10 
+					res.render('weather.ejs', {
+						City : City.toUpperCase(),
+						Temp : cityTemp,
+						Desc : desc,
+						Icon : iconURL
+					});
+					//res.send()
+			});
+		}
+		else {
+			res.sendFile( __dirname + '/404.html')
+		}
+		
 	})
+}
+
+
+// weather response if option Accra is clicked
+app.get('/Accra', (req, res) => {
+	
+	checkWeather('Accra', req ,res)
 	
 });
 
 
 // weather response if option Berlin is clicked
-app.get('/berlin', (req, res) => {
+app.get('/Berlin', (req, res) => {
 	
-	const City = "Berlin";
-	
-	const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Berlin,de&units=metric&APPID=a0562d44b2bd5392dc0f1ef5d322cc65";
-
-	var cityTemp = "";
-	
-	// send get request and receive json from api server and check the status code
-	https.get( apiUrl, (response) => {
-	console.log( response.statusCode )
-
-		
-		response.on("data", (data) => {
-			const weatherData = JSON.parse(data);
-			cityTemp = weatherData.main.temp;
-			res.send(`
-			<div style="${cardCSS}">
-			The temperature in ${City} is:
-			<br/>${cityTemp}
-			<br/>degrees  Celcius
-			<p><a href="/">Click here to go back</a></p>
-			</div>
-			`)
-		});
-	})
+	checkWeather('Berlin', req, res)
 	
 });
 
 
 // weather response if option Cairo is clicked
-app.get('/cairo', (req, res) => {
+app.get('/Cairo', (req, res) => {
 	
-	const City = "Cairo";
-	
-	const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Cairo,eg&units=metric&APPID=a0562d44b2bd5392dc0f1ef5d322cc65";
-
-	var cityTemp = "";
-	
-	// send get request and receive json from api server and check the status code
-	https.get( apiUrl, (response) => {
-	console.log( response.statusCode )
-
-
-		response.on("data", (data) => {
-			const weatherData = JSON.parse(data);
-			cityTemp = weatherData.main.temp;
-			res.send(`
-			<div style="${cardCSS}">
-			The temperature in ${City} is:
-			<br/>${cityTemp}
-			<br/>degrees  Celcius
-			<p><a href="/">Click here to go back</a></p>
-			</div>
-			`)
-		});
-	})
+	checkWeather('Cairo', req, res)
 	
 });
 
 
-// a function to respond to custom searches submited via the search form in the index.html
+// a function to respond to custom searches submitted via the search form in the index.html
 app.post('/', (req, res) => {
 
-	//console.log(req.body.cityName);
-	const City = req.body.cityName
+	const cityInput = req.body.cityName;
 	
-	const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&units=metric&APPID=a0562d44b2bd5392dc0f1ef5d322cc65";
-
-	var cityTemp = "";
-	
-	// send get request and receive json from api server and check the status code
-	https.get( apiUrl, (response) => {
-	console.log( response.statusCode )
-		
-		//only display city if a valid resposne from api server
-		if (response.statusCode == 200){
-			
-			response.on("data", (data) => {
-				const weatherData = JSON.parse(data);
-				cityTemp = weatherData.main.temp;
-				res.send(`
-				<div style="${cardCSS}">
-				The temperature in ${City} is:
-				<br/>${cityTemp}
-				<br/>degrees  Celcius
-				<p><small><a href="/">Click here to go back</a></small></p>
-				</div>
-				`)
-			});
-		}
-		else {
-			res.send(`
-			<div style="${cardCSS}">
-			Sorry, no match found for ${City} at this moment...
-			</div>
-			`);
-		}
-	})
+	checkWeather(cityInput, req, res)
 	
 });
 
@@ -168,5 +101,5 @@ app.post('/', (req, res) => {
 // add 404 page... shortcut, send an image instead
 app.use( (req, res)=> {
 	res.status(404);
-	res.sendFile( __dirname + '\\images\\404.jpg') //using backslash in path due to windows file directory syntax
+	res.sendFile( __dirname + '/assets/images/404.jpg')
 });
